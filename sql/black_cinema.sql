7bd39b8e2d5798a84106c7fced35d6aeda5c6fe5
FjrREPO <jossfajar21@gmail.com>
2024-07-05 11:47:48 +0700

    fix sql

--- sql/black_cinema.sql
+++ sql/black_cinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 01, 2024 at 01:28 PM
+-- Generation Time: Jul 05, 2024 at 04:47 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,18 @@ SET time_zone = "+00:00";
 --
 -- Database: `black_cinema`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_pending_payments` ()   BEGIN
+    UPDATE payment
+    SET status = 'canceled'
+    WHERE status = 'pending' AND expiredPayment <= NOW();
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -114,16 +126,23 @@ CREATE TABLE `payment` (
 --
 
 INSERT INTO `payment` (`id`, `userId`, `movieId`, `createdAt`, `userName`, `userEmail`, `startTime`, `endTime`, `feeAdmin`, `price`, `totalPrice`, `packageName`, `methodPayment`, `promoCode`, `status`, `expiredPayment`, `successPayment`, `room`) VALUES
-(1, 4, 700, '2024-04-30 17:13:49', 'qq', 'qq@gmail.com', '2024-07-01 19:12:00', '2024-07-01 21:26:00', 5000, 1200000, 1205000, '', 'Dana', '', 'canceled', '2024-06-30 10:43:49', '2024-06-30 10:13:49', 0),
-(3, 4, 701, '2024-06-30 18:57:14', 'qq', 'qq@gmail.com', '2024-07-01 18:57:00', '2024-07-01 20:36:00', 5000, 1200000, 1205000, 'reguler', 'Dana', '', 'canceled', '2024-06-30 12:27:14', '2024-06-30 11:57:14', 1),
-(4, 4, 701, '2024-06-30 19:17:21', 'qq', 'qq@gmail.com', '2024-07-03 19:17:00', '2024-07-03 20:56:00', 5000, 1200000, 1205000, 'reguler', 'Dana', '', 'canceled', '2024-06-30 12:47:21', NULL, 2),
-(5, 4, 701, '2024-06-30 20:01:38', 'qq', 'qq@gmail.com', '2024-06-30 20:02:00', '2024-06-30 21:41:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-06-30 13:31:38', NULL, 3),
-(6, 4, 701, '2024-06-30 20:03:11', 'qq', 'qq@gmail.com', '2024-07-02 20:02:00', '2024-07-02 21:41:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-06-30 13:33:11', NULL, 4),
-(7, 4, 701, '2024-06-30 20:03:23', 'qq', 'qq@gmail.com', '2024-07-02 20:02:00', '2024-07-02 21:41:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-06-30 13:33:23', NULL, 5),
-(8, 4, 701, '2024-07-01 19:34:25', 'qq', 'qq@gmail.com', '2024-07-02 20:34:00', '2024-07-02 22:13:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 13:04:25', NULL, 6),
-(9, 4, 701, '2024-07-01 19:35:51', 'qq', 'qq@gmail.com', '2024-07-09 20:34:00', '2024-07-09 22:13:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 13:05:51', NULL, 7),
-(10, 4, 701, '2024-07-01 19:38:52', 'qq', 'qq@gmail.com', '2024-07-02 19:38:00', '2024-07-02 21:17:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 13:08:52', NULL, 8),
-(11, 4, 701, '2024-07-01 19:41:24', 'qq', 'qq@gmail.com', '2024-07-01 19:41:00', '2024-07-01 21:20:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 20:11:24', NULL, 9);
+(23, 4, 701, '2024-07-01 20:40:06', 'qq', 'qq@gmail.com', '2024-07-01 20:40:00', '2024-07-01 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'success', '2024-07-01 21:10:06', '2024-07-04 18:21:36', 1),
+(24, 4, 701, '2024-07-01 20:40:19', 'qq', 'qq@gmail.com', '2024-07-01 20:40:00', '2024-07-01 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'success', '2024-07-01 21:10:19', '2024-07-04 18:33:17', 2),
+(25, 4, 701, '2024-07-01 20:40:20', 'qq', 'qq@gmail.com', '2024-07-01 20:40:00', '2024-07-01 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'success', '2024-07-01 21:10:20', '2024-07-04 18:34:54', 3),
+(26, 4, 701, '2024-07-01 20:40:22', 'qq', 'qq@gmail.com', '2024-07-01 20:40:00', '2024-07-01 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:10:22', NULL, 4),
+(27, 4, 701, '2024-07-01 20:40:27', 'qq', 'qq@gmail.com', '2024-07-16 20:40:00', '2024-07-16 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:10:27', NULL, 5),
+(28, 4, 701, '2024-07-01 20:42:12', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:12:12', NULL, 1),
+(29, 4, 701, '2024-07-01 20:42:27', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:12:27', NULL, 2),
+(30, 4, 701, '2024-07-01 20:43:58', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:13:58', NULL, 3),
+(31, 4, 701, '2024-07-01 20:44:01', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:14:01', NULL, 4),
+(32, 4, 701, '2024-07-01 20:44:02', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:14:02', NULL, 5),
+(33, 4, 701, '2024-07-01 20:44:04', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:14:04', NULL, 6),
+(34, 4, 701, '2024-07-01 20:44:06', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:14:06', NULL, 7),
+(35, 4, 701, '2024-07-01 20:47:30', 'qq', 'qq@gmail.com', '2024-07-24 20:40:00', '2024-07-24 22:19:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-01 21:17:30', NULL, 8),
+(36, 5, 701, '2024-07-04 18:35:36', 'palkon', 'jossfajar22@gmail.com', '2024-07-12 18:35:00', '2024-07-12 20:14:00', 5000, 1200000, 1205000, 'reguler', '', '', 'success', '2024-07-04 19:05:36', '2024-07-04 19:23:36', 1),
+(37, 5, 701, '2024-07-04 18:46:06', 'palkon', 'jossfajar22@gmail.com', '2024-07-04 18:46:00', '2024-07-04 20:25:00', 5000, 1200000, 1205000, 'reguler', '', '', 'success', '2024-07-04 19:16:06', '2024-07-04 19:19:22', 1),
+(38, 4, 701, '2024-07-04 19:32:39', 'qq', 'qq@gmail.com', '2024-07-11 19:32:00', '2024-07-11 21:11:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-04 20:02:39', NULL, 1),
+(39, 4, 700, '2024-07-04 20:03:21', 'qq', 'qq@gmail.com', '2024-07-16 20:03:00', '2024-07-16 22:17:00', 5000, 1200000, 1205000, 'reguler', '', '', 'canceled', '2024-07-04 20:33:21', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -146,7 +165,8 @@ CREATE TABLE `payment_card` (
 
 INSERT INTO `payment_card` (`id`, `numberCard`, `nameCard`, `imageCard`, `categoryInstitue`, `imageQR`) VALUES
 (1, '123434', 'BRI', 'https://cdn3.iconfinder.com/data/icons/banks-in-indonesia-logo-badge/100/BRI-512.png', 'ewallet', 'https://www.techopedia.com/wp-content/uploads/2023/03/aee977ce-f946-4451-8b9e-bba278ba5f13.png'),
-(2, '453564574', 'Dana', 'https://cdn.antaranews.com/cache/1200x800/2022/04/25/dana.jpg', 'ewallet', 'https://www.techopedia.com/wp-content/uploads/2023/03/aee977ce-f946-4451-8b9e-bba278ba5f13.png');
+(2, '453564574', 'Dana', 'https://cdn.antaranews.com/cache/1200x800/2022/04/25/dana.jpg', 'ewallet', 'https://www.techopedia.com/wp-content/uploads/2023/03/aee977ce-f946-4451-8b9e-bba278ba5f13.png'),
+(3, '123', 'as', 'https://alfamart.co.id/storage/page/January2022/CNKpimFu30rDwGT22iJl.jpg', 'ewallet', 'https://alfamart.co.id/storage/page/January2022/CNKpimFu30rDwGT22iJl.jpg');
 
 -- --------------------------------------------------------
 
@@ -197,6 +217,7 @@ CREATE TABLE `user` (
   `user_password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `emailVerified` datetime DEFAULT NULL,
   `user_image` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_telepon` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `user_role` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
@@ -206,11 +227,12 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `user_username`, `user_email`, `user_password`, `emailVerified`, `user_image`, `user_role`, `createdAt`, `updatedAt`) VALUES
-(1, 'a', 'a@gmail.com', '$2y$10$JQ7tC/.vzN9ThUr8x1tpO.EdJ9VljEKQwSF4InQIvhE5cSiQ3fkXq', NULL, 'https://example.com/default_image.jpg', 'admin', '2024-06-29 19:49:52', '2024-06-29 19:50:08'),
-(2, 'as', 'as@gmail.com', '$2y$10$j1Mhkjh3QiRATUxYKonvtOdqKsr5reLUOhB/K.EhU0PGxhoiNb1WO', NULL, 'https://example.com/default_image.jpg', 'admin', '2024-06-30 14:47:15', '2024-06-30 14:47:35'),
-(3, 'palkon', 'wildannoob354@gmail.com', '$2y$10$Kfku74OZb9fTSnc8C6dKfORaA50lna9flcwfB.jtiLjXJxgMFE.pi', NULL, 'https://example.com/default_image.jpg', 'user', '2024-06-30 14:55:23', NULL),
-(4, 'qq', 'qq@gmail.com', '$2y$10$G/y9t9Ms7gAP3mS/4XQG5OfHxhc7m7qGkVoV7rbjiF2Gdaq6Z9teC', NULL, 'https://deepgrouplondon.com/wp-content/uploads/2019/06/person-placeholder-5.png', 'user', '2024-06-30 15:26:59', '2024-07-01 18:00:15');
+INSERT INTO `user` (`user_id`, `user_username`, `user_email`, `user_password`, `emailVerified`, `user_image`, `user_telepon`, `user_role`, `createdAt`, `updatedAt`) VALUES
+(1, 'a', 'a@gmail.com', '$2y$10$JQ7tC/.vzN9ThUr8x1tpO.EdJ9VljEKQwSF4InQIvhE5cSiQ3fkXq', NULL, 'https://example.com/default_image.jpg', NULL, 'admin', '2024-06-29 19:49:52', '2024-06-29 19:50:08'),
+(2, 'as', 'as@gmail.com', '$2y$10$j1Mhkjh3QiRATUxYKonvtOdqKsr5reLUOhB/K.EhU0PGxhoiNb1WO', NULL, 'https://example.com/default_image.jpg', NULL, 'admin', '2024-06-30 14:47:15', '2024-06-30 14:47:35'),
+(3, 'palkon', 'wildannoob354@gmail.com', '$2y$10$Kfku74OZb9fTSnc8C6dKfORaA50lna9flcwfB.jtiLjXJxgMFE.pi', NULL, 'https://example.com/default_image.jpg', NULL, 'user', '2024-06-30 14:55:23', NULL),
+(4, 'qq', 'qq@gmail.com', '$2y$10$G/y9t9Ms7gAP3mS/4XQG5OfHxhc7m7qGkVoV7rbjiF2Gdaq6Z9teC', NULL, 'https://deepgrouplondon.com/wp-content/uploads/2019/06/person-placeholder-5.png', NULL, 'user', '2024-06-30 15:26:59', '2024-07-01 18:00:15'),
+(5, 'palkon', 'jossfajar22@gmail.com', '$2y$10$t.N7twQczIVLAdRKV/HZ7.TwL3X9djPk7Bh.YvOkemQUSWriW3iOG', NULL, 'https://example.com/default_image.jpg', NULL, 'user', '2024-07-04 18:35:23', NULL);
 
 --
 -- Indexes for dumped tables
@@ -296,13 +318,13 @@ ALTER TABLE `movie`
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `payment_card`
 --
 ALTER TABLE `payment_card`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `payment_plan`
@@ -320,7 +342,7 @@ ALTER TABLE `payment_promo`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -350,12 +372,7 @@ DELIMITER $$
 --
 -- Events
 --
-CREATE DEFINER=`root`@`localhost` EVENT `update_payment_status_event` ON SCHEDULE EVERY 10 MINUTE STARTS '2024-07-01 20:28:27' ENDS '2024-07-01 21:28:27' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
-    UPDATE payment
-    SET status = 'canceled'
-    WHERE status = 'pending'
-    AND expiredPayment <= NOW();
-END$$
+CREATE DEFINER=`root`@`localhost` EVENT `cancel_pending_payments` ON SCHEDULE EVERY 10 MINUTE STARTS '2024-07-04 16:58:29' ON COMPLETION NOT PRESERVE ENABLE DO CALL update_pending_payments()$$
 
 DELIMITER ;
 COMMIT;
